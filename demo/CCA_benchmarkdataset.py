96388dcb233b5685b2cc2e20718fdef6a0d6463b
Ze Wang <wangze19910407@gmail.com>
2022-07-05 15:22:30 +0800

    Update CCA_benchmarkdataset.py

---
+++ demo/CCA_benchmarkdataset.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+import sys
+sys.path.append('..')
+
+import time
+
+from SSVEPAnalysisToolbox.datasets.benchmarkdataset import BenchmarkDataset
+from SSVEPAnalysisToolbox.utils.benchmarkpreprocess import preprocess, filterbank, suggested_ch, suggested_weights_filterbank
+from SSVEPAnalysisToolbox.algorithms.cca import SCCA_qr, SCCA_canoncorr, ECCA
+
+weights_filterbank = suggested_weights_filterbank()
+ch_used = suggested_ch()
+harmonic_num = 5
+
+dataset = BenchmarkDataset(path = '/data/2016_Tsinghua_SSVEP_database')
+dataset.regist_preprocess(lambda X: preprocess(X,dataset.srate))
+dataset.regist_filterbank(lambda X: filterbank(X, dataset.srate))
+
+tw = 4 
+sub_idx = 0
+block_idx = 0
+
+test_block, train_block = dataset.leave_one_block_out(block_idx)
+
+ref_sig = dataset.get_ref_sig(tw,harmonic_num)
+X, Y = dataset.get_data_all_stim(sub_idx,test_block,ch_used,tw)
+
+# t_star1 = time.time()
+# Model1 = SCCA_qr(weights_filterbank = weights_filterbank)
+# Model1.fit(ref_sig = ref_sig)
+# t1_train = time.time() - t_star1
+# t_star1 = time.time()
+# Y_pred1 = Model1.predict(X)
+# t1_test = time.time() - t_star1
+
+# t_star2 = time.time()
+# Model2 = SCCA_canoncorr(weights_filterbank = weights_filterbank)
+# Model2.fit(ref_sig = ref_sig)
+# t2_train = time.time() - t_star2
+# t_star2 = time.time()
+# Y_pred2 = Model2.predict(X)
+# t2_test = time.time() - t_star2
+
+ref_sig = dataset.get_ref_sig(tw,harmonic_num)
+X_train, Y_train = dataset.get_data_all_stim(sub_idx,train_block,ch_used,tw,
+                                            shuffle=True)
+
+t_star3 = time.time()
+Model3 = ECCA(weights_filterbank = weights_filterbank)
+Model3.fit(X_train, Y_train, ref_sig = ref_sig)
+t3_train = time.time() - t_star3
+t_star3 = time.time()
+Y_pred3 = Model3.predict(X)
+t3_test = time.time() - t_star3
+
+
+
+
