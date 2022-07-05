af4e48bf4ec37ebb5c23f18a9d65526345983503
Ze Wang <wangze19910407@gmail.com>
2022-07-05 15:22:58 +0800

    eCCA pass pre test

--- SSVEPAnalysisToolbox/algorithms/cca.py
+++ SSVEPAnalysisToolbox/algorithms/cca.py
@@ -152,7 +152,13 @@ def _r_cca_qr_withUV(X: ndarray,
     harmonic_num = Y_R[0].shape[-1]
     stimulus_num = len(Y_Q)
     
-    Y = [qr_inverse(Y_Q[i],Y_R[i],Y_P[i]).T for i in range(len(Y_Q))]
+    Y = [qr_inverse(Y_Q[i],Y_R[i],Y_P[i]) for i in range(len(Y_Q))]
+    if len(Y[0].shape)==2: # reference
+        Y = [Y_tmp.T for Y_tmp in Y]
+    elif len(Y[0].shape)==3: # template
+        Y = [np.transpose(Y_tmp, (0,2,1)) for Y_tmp in Y]
+    else:
+        raise ValueError('Unknown data type')
     
     R = np.zeros((filterbank_num, stimulus_num))
     
@@ -298,7 +304,13 @@ def _r_cca_qr(X: ndarray,
     harmonic_num = Y_R[0].shape[-1]
     stimulus_num = len(Y_Q)
     
-    Y = [qr_inverse(Y_Q[i],Y_R[i],Y_P[i]).T for i in range(len(Y_Q))]
+    Y = [qr_inverse(Y_Q[i],Y_R[i],Y_P[i]) for i in range(len(Y_Q))]
+    if len(Y[0].shape)==2: # reference
+        Y = [Y_tmp.T for Y_tmp in Y]
+    elif len(Y[0].shape)==3: # template
+        Y = [np.transpose(Y_tmp, (0,2,1)) for Y_tmp in Y]
+    else:
+        raise ValueError('Unknown data type')
     
     # R1 = np.zeros((filterbank_num,stimulus_num))
     # R2 = np.zeros((filterbank_num,stimulus_num))
@@ -575,13 +587,13 @@ class ECCA(BaseModel):
                  n_component: Optional[int] = 1,
                  n_jobs: Optional[int] = None,
                  weights_filterbank: Optional[List[float]] = None,
-                 force_output_UV: Optional[bool] = False,
+                 # force_output_UV: Optional[bool] = False,
                  update_UV: Optional[bool] = True):
         """
         Special Parameters
         ----------
-        force_output_UV : Optional[bool] 
-            Whether store U and V. Default is False
+        # force_output_UV : Optional[bool] 
+        #     Whether store U and V. Default is False
         update_UV: Optional[bool]
             Whether update U and V in next time of applying "predict" 
             If false, and U and V have not been stored, they will be stored
@@ -591,7 +603,7 @@ class ECCA(BaseModel):
                          n_component = n_component,
                          n_jobs = n_jobs,
                          weights_filterbank = weights_filterbank)
-        self.force_output_UV = force_output_UV
+        # self.force_output_UV = force_output_UV
         self.update_UV = update_UV
         
         self.model['U1'] = None
@@ -635,44 +647,65 @@ class ECCA(BaseModel):
         filterbank_num = template_sig[0].shape[0]
         stimulus_num = len(template_sig)
         channel_num = template_sig[0].shape[1]
-        harmonic_num = ref_sig[0].shape[1]
+        harmonic_num = ref_sig[0].shape[0]
         n_component = self.n_component
         U3 = np.zeros((filterbank_num, stimulus_num, channel_num, n_component))
         V3 = np.zeros((filterbank_num, stimulus_num, harmonic_num, n_component))
         for filterbank_idx in range(filterbank_num):
             for stim_idx, (template_sig_single, ref_sig_single) in enumerate(zip(template_sig,ref_sig)):
-                U, V, _ = canoncorr(template_sig_single.T, ref_sig_single.T, True)
-                U3[filterbank_idx, stim_idx, :, :] = U
-                V3[filterbank_idx, stim_idx, :, :] = V
+                U, V, _ = canoncorr(template_sig_single[filterbank_idx,:,:].T, ref_sig_single.T, True)
+                U3[filterbank_idx, stim_idx, :, :] = U[:channel_num,:n_component]
+                V3[filterbank_idx, stim_idx, :, :] = V[:harmonic_num,:n_component]
         self.model['U3'] = U3
         self.model['V3'] = V3
             
         
     def predict(self,
                 X: List[ndarray]) -> List[int]:
-        # weights_filterbank = self.model['weights_filterbank']
-        # if weights_filterbank is None:
-        #     weights_filterbank = [1 for _ in range(X[0].shape[0])]
-        # weights_filterbank = np.expand_dims(np.array(weights_filterbank),1).T
-        # n_component = self.n_component
-        # Y_Q = self.model['ref_sig_Q']
-        # Y_R = self.model['ref_sig_R']
-        # Y_P = self.model['ref_sig_P']
-        # force_output_UV = self.force_output_UV
-        # update_UV = self.update_UV
+        weights_filterbank = self.model['weights_filterbank']
+        if weights_filterbank is None:
+            weights_filterbank = [1 for _ in range(X[0].shape[0])]
+        weights_filterbank = np.expand_dims(np.array(weights_filterbank),1).T
+        n_component = self.n_component
+        update_UV = self.update_UV
         
-        # if update_UV or self.model['U'] is None or self.model['V'] is None:
-        #     if force_output_UV or not update_UV:
-        #         r, U, V = zip(*Parallel(n_jobs=self.n_jobs)(delayed(partial(_r_cca_qr, n_component=n_component, Y_Q=Y_Q, Y_R=Y_R, Y_P=Y_P, force_output_UV=True))(a) for a in X))
-        #         self.model['U'] = U
-        #         self.model['V'] = V
-        #     else:
-        #         r = Parallel(n_jobs=self.n_jobs)(delayed(partial(_r_cca_qr, n_component=n_component, Y_Q=Y_Q, Y_R=Y_R, Y_P=Y_P, force_output_UV=False))(a) for a in X)
-        # else:
-        #     U = self.model['U']
-        #     V = self.model['V']
-        #     r = Parallel(n_jobs=self.n_jobs)(delayed(partial(_r_cca_qr_withUV, Y_Q=Y_Q, Y_R=Y_R, Y_P=Y_P))(X=a, U=u, V=v) for a, u, v in zip(X,U,V))
+        ref_sig_Q = self.model['ref_sig_Q']
+        ref_sig_R = self.model['ref_sig_R']
+        ref_sig_P = self.model['ref_sig_P']
         
-        # Y_pred = [int(np.argmax(weights_filterbank @ r_single, axis = 1)) for r_single in r]
+        template_sig_Q = self.model['template_sig_Q'] 
+        template_sig_R = self.model['template_sig_R'] 
+        template_sig_P = self.model['template_sig_P'] 
         
-        # return Y_pred
+        U3 = self.model['U3'] 
+        V3 = self.model['V3'] 
+        
+        # r1
+        if update_UV or self.model['U1'] is None or self.model['V1'] is None:
+            r1, U1, V1 = zip(*Parallel(n_jobs=self.n_jobs)(delayed(partial(_r_cca_qr, n_component=n_component, Y_Q=ref_sig_Q, Y_R=ref_sig_R, Y_P=ref_sig_P, force_output_UV=True))(a) for a in X))
+            self.model['U1'] = U1
+            self.model['V1'] = V1
+        else:
+            U1 = self.model['U1']
+            V1 = self.model['V1']
+            r1 = Parallel(n_jobs=self.n_jobs)(delayed(partial(_r_cca_qr_withUV, Y_Q=ref_sig_Q, Y_R=ref_sig_R, Y_P=ref_sig_P))(X=a, U=u, V=v) for a, u, v in zip(X,U1,V1))
+        
+        # r2
+        if update_UV or self.model['U2'] is None:
+            _, U2, _ = zip(*Parallel(n_jobs=self.n_jobs)(delayed(partial(_r_cca_qr, n_component=n_component, Y_Q=template_sig_Q, Y_R=template_sig_R, Y_P=template_sig_P, force_output_UV=True))(a) for a in X))
+            self.model['U2'] = U2
+        r2 = Parallel(n_jobs=self.n_jobs)(delayed(partial(_r_cca_qr_withUV, Y_Q=template_sig_Q, Y_R=template_sig_R, Y_P=template_sig_P))(X=a, U=u, V=v) for a, u, v in zip(X,U2,U2))
+        
+        # r3
+        r3 = Parallel(n_jobs=self.n_jobs)(delayed(partial(_r_cca_qr_withUV, Y_Q=template_sig_Q, Y_R=template_sig_R, Y_P=template_sig_P))(X=a, U=u, V=v) for a, u, v in zip(X,U1,U1))
+        
+        # r4
+        r4 = Parallel(n_jobs=self.n_jobs)(delayed(partial(_r_cca_qr_withUV, Y_Q=template_sig_Q, Y_R=template_sig_R, Y_P=template_sig_P, U=U3, V=U3))(X=a) for a in X)
+        
+        
+        Y_pred = [int( np.argmax( weights_filterbank @ (np.sign(r1_single) * np.square(r1_single) + 
+                                                        np.sign(r2_single) * np.square(r2_single) +
+                                                        np.sign(r3_single) * np.square(r3_single) +
+                                                        np.sign(r4_single) * np.square(r4_single)))) for r1_single, r2_single, r3_single, r4_single in zip(r1, r2, r3, r4)]
+        
+        return Y_pred
