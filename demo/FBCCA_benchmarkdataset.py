5880188648bd0b962c988090235dd570f1ccba05
Ze Wang <wangze19910407@gmail.com>
2022-07-05 12:03:10 +0800

    Update FBCCA_benchmarkdataset.py

--- demo/FBCCA_benchmarkdataset.py
+++ demo/FBCCA_benchmarkdataset.py
@@ -30,14 +30,18 @@ X, Y = dataset.get_data_all_stim(sub_idx,test_block,ch_used,tw,
 t_star1 = time.time()
 Model1 = SCCA_qr(weights_filterbank = weights_filterbank)
 Model1.fit(ref_sig = ref_sig)
+t1_train = time.time() - t_star1
+t_star1 = time.time()
 Y_pred1 = Model1.predict(X)
-t1 = time.time() - t_star1
+t1_test = time.time() - t_star1
 
 t_star2 = time.time()
 Model2 = SCCA_canoncorr(weights_filterbank = weights_filterbank)
 Model2.fit(ref_sig = ref_sig)
+t2_train = time.time() - t_star2
+t_star2 = time.time()
 Y_pred2 = Model2.predict(X)
-t2 = time.time() - t_star2
+t2_test = time.time() - t_star2
 
 
 
