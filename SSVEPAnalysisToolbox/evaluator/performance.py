94b0800807101d2903376bd882f4ee8f39b765df
Ze Wang <wangze19910407@gmail.com>
2022-07-08 13:28:39 +0800

    Add cal_performance_onedataset_individual_diffsiglen

--- SSVEPAnalysisToolbox/evaluator/performance.py
+++ SSVEPAnalysisToolbox/evaluator/performance.py
@@ -5,6 +5,71 @@ from numpy import ndarray
 
 import numpy as np
 
+def cal_performance_onedataset_individual_diffsiglen(evaluator: object,
+                                                     dataset_idx: int,
+                                                     tw_seq: List[float],
+                                                     train_or_test: str) -> Tuple[ndarray, ndarray]:
+    """
+    Calculate acc and ITR for one dataset
+    Evaluations will be carried out on each subject and each signal length
+    Under each subject and each signal length, there may be several trials. Acc and itr values of all these trials will be averaged.
+
+    Parameters
+    ----------
+    evaluator : object
+        evaluator
+    dataset_idx : int
+        dataset index
+    tw_seq : List[float]
+        List of signal length
+    train_or_test : str
+        Calculate performance of training or testing performance
+
+    Returns
+    -------
+    acc_store: ndarray
+        Classification accuracy
+        Shape: (method_num, subject_num, signal_len_num)
+    itr_store: ndarray
+        Classification ITR
+        Shape: (method_num, subject_num, signal_len_num)
+    """
+    if train_or_test.lower() == "train":
+        idx = 0
+    elif train_or_test.lower() == "test":
+        idx = 1
+    else:
+        raise ValueError("Unknown train_or_test type. It must be 'train' or 'test'")
+    dataset_container = evaluator.dataset_container
+    model_container = evaluator.model_container
+    sub_num = len(dataset_container[dataset_idx].subjects)
+    t_break = dataset_container[dataset_idx].t_break
+    N = dataset_container[dataset_idx].stim_info['stim_num']
+    
+    acc_store = np.zeros((len(model_container),sub_num, len(tw_seq)))
+    itr_store = np.zeros((len(model_container),sub_num, len(tw_seq)))
+    for sub_idx in range(sub_num):
+        for tw_idx, tw in enumerate(tw_seq):
+            trial_info = {'dataset_idx':[dataset_idx],
+                          'sub_idx':[sub_idx],
+                          'tw':tw}
+            trial_idx = evaluator.search_trial_idx(train_or_test, trial_info)
+            
+            for method_idx in range(len(model_container)):
+                tmp_acc = []
+                tmp_itr = []
+                for i in trial_idx:
+                    performance_container = [evaluator.performance_container[i][method_idx]]
+                    tmp_acc.append(cal_acc_trials(train_or_test, performance_container))
+                    t_latency = evaluator.trial_container[i][idx].t_latency[dataset_idx]
+                    tw = evaluator.trial_container[i][idx].tw
+                    if t_latency is None:
+                        t_latency = dataset_container[dataset_idx].default_t_latency
+                    tmp_itr.append(cal_itr_trials(train_or_test, performance_container, tw, t_break, t_latency, N))
+                acc_store[method_idx,sub_idx,tw_idx] = mean(tmp_acc)
+                itr_store[method_idx,sub_idx,tw_idx] = mean(tmp_itr)
+    return acc_store, itr_store
+
 def cal_itr_trials(train_or_test: str,
                    performance_container: list,
                    tw,t_break,t_latency,N) -> float:
