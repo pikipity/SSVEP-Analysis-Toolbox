37c73a1b56cf648ac89d0ab817ea528ec63bce39
Ze Wang <wangze19910407@gmail.com>
2022-07-08 19:45:57 +0800

    Create betadataset.py

---
+++ SSVEPAnalysisToolbox/datasets/betadataset.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+B. Liu, X. Huang, Y. Wang, X. Chen, and X. Gao, “BETA: A large benchmark database toward SSVEP-BCI application,” Front. Neurosci., vol. 14, p. 627, 2020.
+"""
+
