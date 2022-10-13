ed98f507e214c414dfedfabab6dde954a366e076
Ze Wang <wangze19910407@gmail.com>
2022-10-13 09:37:19 +0800

    Update __init__.py

--- SSVEPAnalysisToolbox/datasets/__init__.py
+++ SSVEPAnalysisToolbox/datasets/__init__.py
@@ -4,6 +4,6 @@ from .basedataset import BaseDataset
 from .benchmarkdataset import BenchmarkDataset
 from .betadataset import BETADataset
 from .eldbetadataset import ELDBETADataset
-from nakanishidataset import NakanishiDataset
+from .nakanishidataset import NakanishiDataset
 from .openbmidataset import openBMIDataset
-from wearabledataset import WearableDataset_dry, WearableDataset_wet
+from .wearabledataset import WearableDataset_dry, WearableDataset_wet
