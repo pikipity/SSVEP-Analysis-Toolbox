b3cf0db3982f5d0042e81c9903bbaef9869d983d
TrellixVulnTeam <charles.mcfarland@trellix.com>
2023-01-06 04:46:34 +0000

    Adding tarfile member sanitization to extractall()

--- SSVEPAnalysisToolbox/datasets/betadataset.py
+++ SSVEPAnalysisToolbox/datasets/betadataset.py
@@ -112,7 +112,26 @@ class BETADataset(BaseDataset):
                 download_single_file(source_url, desertation)
             
                 with tarfile.open(desertation,'r') as archive:
-                    archive.extractall(subject.path)
+                    def is_within_directory(directory, target):
+                        
+                        abs_directory = os.path.abspath(directory)
+                        abs_target = os.path.abspath(target)
+                    
+                        prefix = os.path.commonprefix([abs_directory, abs_target])
+                        
+                        return prefix == abs_directory
+                    
+                    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                    
+                        for member in tar.getmembers():
+                            member_path = os.path.join(path, member.name)
+                            if not is_within_directory(path, member_path):
+                                raise Exception("Attempted Path Traversal in Tar File")
+                    
+                        tar.extractall(path, members, numeric_owner) 
+                        
+                    
+                    safe_extract(archive, subject.path)
                     
                 os.remove(desertation)
             except:
